5aa61e6f12e39ff5703cb47eb4b6a06df81269c0
Joe Franck Bolla <joe_bolla@yahoo.com>
2018-04-20 00:14:16 +0100

    last article changes.

---
+++ elearning/elearning.model/ViewModels/FileUploadVm.cs
@@ -0,0 +1,10 @@
+﻿
+namespace elearning.model.ViewModels
+{
+    public class FileUploadVm
+    {
+        public int MaxFileUpload { get; set; }
+
+        public string PopupTitle { get; set; }
+    }
+}
